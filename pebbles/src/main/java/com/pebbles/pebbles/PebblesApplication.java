24c8beced36b9a678dadb501e7a4e6f2ca0f4f2d
Vinu Aravindh <68106433+vinuaravindh@users.noreply.github.com>
2025-08-17 15:48:12 +0400

    Setup Spring boot

---
+++ pebbles/src/main/java/com/pebbles/pebbles/PebblesApplication.java
@@ -0,0 +1,13 @@
+package com.pebbles.pebbles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PebblesApplication.class, args);
+	}
+
+}
---
+++ pebbles/src/test/java/com/pebbles/pebbles/PebblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pebbles.pebbles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
